bf158189c1b6aa359588122a3f55feac0e9fbbe4
Mouhammad Nour <mouhammadcourse@gmail.com>
2025-12-04 02:55:12 +0300

    Fetch the games

---
+++ src/services/api-client.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: 'https://api.rawg.io/api',
+    params: {
+        key: '10ad73e1b11f40d9b8c02f2cb0fb9c15'
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import axios from 'axios'
+
+// export default axios.create({
+//     baseURL: 'https://api.rawg.io/api',
+//     params : {
+//         key: '10ad73e1b11f40d9b8c02f2cb0fb9c15'
+//     }
+// })
