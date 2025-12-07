30588a373b615889f1aa668c2962a23e07bacaba
Mouhammad Nour <mouhammadcourse@gmail.com>
2025-12-07 06:24:00 +0300

    Customize the theme to get darker grays

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,9 @@ interface Props {
 
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 9, 10];
+  const skeletons = [
+    1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,21
+  ];
   return (
     <>
       {error && <Text>{error}</Text>}
